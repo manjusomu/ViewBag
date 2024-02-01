9a626a60497b89aa0765282d94948af01bb0ea44
manjusomu <manjusomu2001@gmail.com>
2024-02-01 17:50:51 +0530

    View Bag

---
+++ WebApptask3/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApptask3.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<string> list = new List<string>()
+            {
+                "Ravi","Ram","Janu","Guna" };
+            ViewBag.namelist = list;
+            return View();
+        }
+      
+        public ActionResult RedirectDemo()
+        {
+
+
+            TempData["msg"] = "Welcome to Customer Page";
+            return View();
+        }
+        public ActionResult DisplayMessage() 
+        {
+
+            TempData["Dmsg"] = "Customer Details";
+            return View();
+        }
+    }
+}
---
+++ WebApptask3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApptask3.MvcApplication" Language="C#" %>
